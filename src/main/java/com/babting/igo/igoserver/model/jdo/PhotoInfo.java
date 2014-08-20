7af0ad271f2951cefb339dbbfd2b6e00c22172f9
leechanguk <leechanguk@192.168.2.23>
2014-08-20 14:03:45 +0900

    .

---
+++ src/main/java/com/babting/igo/igoserver/model/jdo/PhotoInfo.java
@@ -0,0 +1,20 @@
+package com.babting.igo.igoserver.model.jdo;
+
+public class PhotoInfo {
+	private String title;
+	private String url;
+	
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+}
